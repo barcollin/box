c2280359d08f924552dc34214cffa40904623c36
Jay Bird <jubilates@hotmail.com>
2024-10-18 13:39:44 +0200

    add Manager struct to  manager package

---
+++ manager/manager.go
@@ -0,0 +1,16 @@
+package manager
+
+import (
+	"github.com/barcollin/box/task"
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+)
+
+type Manager struct {
+	Pending       queue.Queue
+	TaskDb        map[string][]*task.Task
+	EventDb       map[string][]*task.TaskEvent
+	Workers       []string
+	WorkerTaskMap map[string][]uuid.UUID
+	TaskWorkerMap map[uuid.UUID]string
+}
