9c8f78ebcbd29cc44374a12a19ec254a76694037
Jay Bird <jubilates@hotmail.com>
2024-10-18 13:40:11 +0200

    add worker struct to the worker package

---
+++ worker/worker.go
@@ -0,0 +1,20 @@
+package worker
+
+import (
+	"fmt"
+
+	"github.com/barcollin/box/task"
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+)
+
+type Worker struct {
+	Name      string
+	Queue     queue.Queue
+	Db        map[uuid.UUID]*task.Task
+	TaskCount int
+}
+
+func (w *Worker) CollectStats() {
+	fmt.Println("i will collect stats")
+}
